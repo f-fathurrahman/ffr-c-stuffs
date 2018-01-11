f4174a73c9492b521f5c21c0f32dda90ada2c110
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-11 17:26:38 +0700

    Add test_const

---
+++ test_const.c
@@ -0,0 +1,12 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+  const double PI = M_PI;
+  printf("PI = %18.10f\n", PI);
+
+  const double FOURPI = 4.0*PI;
+  printf("FOURPI = %18.10f\n", FOURPI);
+  return 0;
+}
