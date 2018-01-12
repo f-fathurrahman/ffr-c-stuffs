34dd69b6076f92d34aa8e82b2b55a5597ab0a5aa
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2018-01-12 13:27:04 +0700

    Add test_struct

---
+++ test_struct.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct MyStruct {
+  int N;
+  double *data;
+};
+
+void init_MyStruct( struct MyStruct *a, int N )
+{
+  (*a).N = N;
+  
+  (*a).data = malloc( N*sizeof(double) );
+  int i;
+  for( i = 0; i < N; i++ ) {
+    (*a).data[i] = 1.2;
+  }
+  printf("Pass here\n");
+}
+
+void info_MyStruct( struct MyStruct a )
+{
+  printf("N = %d\n", a.N );
+  int i;
+  for( i = 0; i < a.N; i++ ) {
+    printf("a[%d] = %f\n", i, a.data[i]);
+  }
+}
+
+int main()
+{
+  printf("Hello from ffr\n");
+  struct MyStruct a;
+  init_MyStruct(&a,3);
+  info_MyStruct(a);
+
+  return 0;
+}
